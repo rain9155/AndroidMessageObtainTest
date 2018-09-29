40b5b7c6ea136b01150634f5cdc9f4a06bfbe963
rain <rain@email.com>
2018-09-29 20:05:44 +0800

    添加一张图片

---
+++ app/src/main/java/com/example/hy/androidmessageobtaintest/apkMessage/PMAppAdapter.java
@@ -0,0 +1,9 @@
+package com.example.hy.androidmessageobtaintest.apkMessage;
+
+import android.widget.BaseAdapter;
+
+/**
+ * Created by 陈健宇 at 2018/9/29
+ */
+public class PMAppAdapter extends BaseAdapter {
+}
---
+++ app/src/main/java/com/example/hy/androidmessageobtaintest/apkMessage/PMAppInfo.java
@@ -0,0 +1,8 @@
+package com.example.hy.androidmessageobtaintest.apkMessage;
+
+/**
+ * 封装apk的基本信息
+ * Created by 陈健宇 at 2018/9/29
+ */
+public class PMApkInfo {
+}
